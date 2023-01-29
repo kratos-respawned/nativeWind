03e845a6a6fd90b93de31172df424ecbde924d2a
Gaurav Bhandari <sbhandari1907@protonmail.com>
2023-01-29 20:53:31 +0530

    implemented flatlist

--- App.tsx
+++ App.tsx
@@ -1,48 +1,53 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
-  const [userName, setUserName] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
   return (
-    <View className="w-full h-full grid justify-center bg-gray-900">
-      <StatusBar style="auto" />
-      <Text className="text-violet-500 text-4xl  font-bold text-center ">
-        Kratos
-      </Text>
-      <TextInput
-        className="w-9/12 h-10 px-4 bg-gray-800 text-white rounded-md mx-auto my-7"
-        placeholder="Username"
-        value={userName}
-        onChangeText={(text) => setUserName(text)}
-        placeholderTextColor={"#9CA3AF"}
-      />
-      <TextInput
-        className="w-9/12 h-10 px-4 bg-gray-800 text-white rounded-md mx-auto "
-        placeholder="Password"
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-        secureTextEntry={true}
-        selectTextOnFocus={true}
-        placeholderTextColor={"#9CA3AF"}
-      />
-      <View className="mx-auto mt-5 w-1/2">
-        <Button
-          color={"#8b5cf6"}
-          title="Login"
-          onPress={() => {
-            if (userName === "" && password === "") {
-              alert("please fill all fields");
-              return;
-            }
-            if (userName === "admin" && password === "admin") {
-              alert("Login Success");
-              return;
-            }
-          }}
+    <View className="flex pb-10">
+      <StatusBar hidden />
+      <View className="flex flex-row items-center bg-sky-500 py-3 gap-5 px-5 shadow-lg shadow-black">
+        <Image
+          source={require("./assets/icon.png")}
+          className="h-16 w-16 rounded-full "
         />
+        <View className="flex-1 ">
+          <Text className="text-xl font-medium text-white">Group Chat</Text>
+          <Text className="text-sm text-sky-900">5 members</Text>
+        </View>
       </View>
+
+      <FlatList
+        data={Array(10).fill(0)}
+        renderItem={({ item, index }) => <Chat item={index} />}
+        keyExtractor={(_, index) => index.toString()}
+        scrollEnabled
+        ListFooterComponent={() => <View className="h-20" />}
+      />
     </View>
   );
 }
+
+const Chat = ({ item }: { item: number }) => {
+  return (
+    <View className="flex flex-row items-center  gap-5 py-2 border-b border-blue-900">
+      <Image
+        source={require("./assets/icon.png")}
+        className="h-16 w-16 rounded-full "
+      />
+      <View className="flex-1 ">
+        <Text className="text-xl font-medium text-blue-900">XYZ</Text>
+        <Text className="text-sm text-sky-900">{item} members</Text>
+      </View>
+    </View>
+  );
+};
