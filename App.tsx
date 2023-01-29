0aaf293d4a2fedd9ce00c37188123b82294258d0
Gaurav Bhandari <sbhandari1907@protonmail.com>
2023-01-29 20:00:58 +0530

    form done

--- App.tsx
+++ App.tsx
@@ -1,13 +1,48 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [userName, setUserName] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   return (
     <View className="w-full h-full grid justify-center bg-gray-900">
-      <Text className="text-violet-500 text-2xl font-bold text-center">
-        NativeWind + <Text className="text-white">Expo</Text>
-      </Text>
       <StatusBar style="auto" />
+      <Text className="text-violet-500 text-4xl  font-bold text-center ">
+        Kratos
+      </Text>
+      <TextInput
+        className="w-9/12 h-10 px-4 bg-gray-800 text-white rounded-md mx-auto my-7"
+        placeholder="Username"
+        value={userName}
+        onChangeText={(text) => setUserName(text)}
+        placeholderTextColor={"#9CA3AF"}
+      />
+      <TextInput
+        className="w-9/12 h-10 px-4 bg-gray-800 text-white rounded-md mx-auto "
+        placeholder="Password"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry={true}
+        selectTextOnFocus={true}
+        placeholderTextColor={"#9CA3AF"}
+      />
+      <View className="mx-auto mt-5 w-1/2">
+        <Button
+          color={"#8b5cf6"}
+          title="Login"
+          onPress={() => {
+            if (userName === "" && password === "") {
+              alert("please fill all fields");
+              return;
+            }
+            if (userName === "admin" && password === "admin") {
+              alert("Login Success");
+              return;
+            }
+          }}
+        />
+      </View>
     </View>
   );
 }
